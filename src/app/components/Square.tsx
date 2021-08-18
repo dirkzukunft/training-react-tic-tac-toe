10d72163edd65dd3b4e58be9a8e2ec4680e00355
dirkzukunft <git@qf3.de>
2021-08-18 10:17:29 +0200

    Add destructuring of props

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -5,10 +5,10 @@ type SquareProps = {
   onClick: () => void;
 };
 
-export default function Square(props: SquareProps): JSX.Element {
+export default function Square({ value, onClick }: SquareProps): JSX.Element {
   return (
-    <button className="square" onClick={() => props.onClick()}>
-      {props.value}
+    <button className="square" onClick={() => onClick()}>
+      {value}
     </button>
   );
 }
